d39e75995cd9220e90339b50a0114cb48bd39988
Eunah323 <unapretty@gmail.com>
2022-04-29 20:00:47 +0900

    Initial commit

---
+++ src/main/java/com/sparta/chorok/ChorokApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.chorok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChorokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChorokApplication.class, args);
+    }
+
+}
